64f1922c5b950198f37b82e0fb4b424dca88907c
Leo.Kuo <et282523@hotmail.com>
2019-06-03 14:22:25 +0800

    河內塔 assert

--- Other/HanoiTowerTest.php
+++ Other/HanoiTowerTest.php
@@ -8,7 +8,8 @@ class HanoiTowerTest extends TestCase
         $solution = new HanoiTower();
         $n = 1;
         $solution->move($n, 'A', 'B', 'C');
-        $this->assertTrue(true);
+        $result = $solution->getResult();
+        $this->assertEquals("1B", $result);
     }
 
     public function testSample2()
@@ -17,6 +18,8 @@ class HanoiTowerTest extends TestCase
         $n = 2;
         $solution->move($n, 'A', 'B', 'C');
         $this->assertTrue(true);
+        $result = $solution->getResult();
+        $this->assertEquals("1C,2B,1B", $result);
     }
 
     public function testSample3()
@@ -24,7 +27,9 @@ class HanoiTowerTest extends TestCase
         $solution = new HanoiTower();
         $n = 3;
         $solution->move($n, 'A', 'B', 'C');
-        $this->assertTrue(true);
+
+        $result = $solution->getResult();
+        $this->assertEquals("1B,2C,1C,3B,1A,2B,1B", $result);
     }
 
     public function testSample4()
@@ -32,7 +37,9 @@ class HanoiTowerTest extends TestCase
         $solution = new HanoiTower();
         $n = 4;
         $solution->move($n, 'A', 'B', 'C');
-        $this->assertTrue(true);
+
+        $result = $solution->getResult();
+        $this->assertEquals("1C,2B,1B,3C,1A,2C,1C,4B,1B,2A,1A,3B,1C,2B,1B", $result);
     }
 
     public function testSample5()
@@ -42,12 +49,21 @@ class HanoiTowerTest extends TestCase
         $solution->move($n, 'A', 'B', 'C');
         $this->assertTrue(true);
     }
+
+    public function testSample10()
+    {
+        $solution = new HanoiTower();
+        $n = 10;
+        $solution->move($n, 'A', 'B', 'C');
+        $this->assertTrue(true);
+    }
 }
 
 class HanoiTower
 {
-    private $step = 1;
-    function move($n, $origin, $target, $tmp)
+    private $step_no = 1;
+    private $results = [];
+    public function move($n, $origin, $target, $tmp)
     {
         // 把其他的盤子搬到暫存柱
         if ($n - 1 > 0) {
@@ -55,12 +71,18 @@ class HanoiTower
         }
 
         // 把最大的盤子直接搬到目標柱
-        echo "{$this->step}. 把 $n 從 $origin -> $target\n";
-        $this->step++;
+        echo "{$this->step_no}. 把 $n 從 $origin -> $target\n";
+        $this->results[] = "{$n}{$target}";
+        $this->step_no++;
 
         // 把其他盤子搬回來目標柱
         if ($n - 1 > 0) {
             $this->move($n - 1, $tmp, $target, $origin);
         }
+    }
+
+    public function getResult()
+    {
+        return implode(",", $this->results);
     }
 }
