4861ea752dc75febbb9be729dec83d4ac55188a6
Leo.Kuo <et282523@hotmail.com>
2019-06-03 13:05:00 +0800

    1. Two Sum

---
+++ Q1_TwoSum.php
@@ -0,0 +1,36 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class Q1_TwoSum extends TestCase
+{
+    public function testSample()
+    {
+        $solution = new Solution();
+        $nums = [2, 7, 11, 15];
+        $target = 9;
+
+        $response = $solution->twoSum($nums, $target);
+        $this->assertEquals([0, 1], $response);
+    }
+}
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        $num = count($nums);
+        for ($i=0; $i<$num-1; $i++) {
+            for ($j=$i+1; $j<$num; $j++) {
+                if ($nums[$i] + $nums[$j] == $target) {
+                    return [$i, $j];
+                }
+            }
+        }
+    }
+}
