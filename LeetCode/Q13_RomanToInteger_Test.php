21678f3f4c7232fab0c4c71fd6268cb10c2b6394
Leo.Kuo <et282523@hotmail.com>
2019-08-04 23:36:58 +0800

    Q13 - submit 1 - Wrong Answer (沒考慮到 2000)

---
+++ LeetCode/Q13_RomanToInteger_Test.php
@@ -0,0 +1,102 @@
+<?php
+namespace LeetCode\Q13;
+use PHPUnit\Framework\TestCase;
+
+class Q13_RomanToInteger_Test extends TestCase
+{
+    /**
+     * @var Solution
+     */
+    private $solution;
+
+    public function setUp()
+    {
+        $this->solution = new Solution();
+    }
+
+    public function test_III應該回傳3()
+    {
+        $roman = "III";
+        $response = $this->solution->romanToInt($roman);
+        $this->assertEquals(3, $response);
+    }
+
+    public function test_IV應該回傳4()
+    {
+        $roman = "IV";
+        $response = $this->solution->romanToInt($roman);
+        $this->assertEquals(4, $response);
+    }
+
+    public function test_VI應該回傳6()
+    {
+        $roman = "IV";
+        $response = $this->solution->romanToInt($roman);
+        $this->assertEquals(4, $response);
+    }
+
+    public function test_LVIII應該回傳58()
+    {
+        $roman = "LVIII";
+        $response = $this->solution->romanToInt($roman);
+        $this->assertEquals(58, $response);
+    }
+
+    public function test_MCMXCIV應該回傳1994()
+    {
+        $roman = "MCMXCIV";
+        $response = $this->solution->romanToInt($roman);
+        $this->assertEquals(1994, $response);
+    }
+}
+
+class Solution
+{
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    public function romanToInt($s)
+    {
+        $values = [
+            "VIII" => 8,
+            "LXXX" => 80,
+            "DCCC" => 800,
+            "III" => 3,
+            "VII" => 7,
+            "XXX" => 30,
+            "LXX" => 70,
+            "CCC" => 300,
+            "DCC" => 700,
+            "II" => 2,
+            "IV" => 4,
+            "VI" => 6,
+            "IX" => 9,
+            "XX" => 20,
+            "XL" => 40,
+            "LX" => 60,
+            "XC" => 90,
+            "CC" => 200,
+            "CD" => 400,
+            "DC" => 600,
+            "CM" => 900,
+            "I" => 1,
+            "V" => 5,
+            "X" => 10,
+            "L" => 50,
+            "C" => 100,
+            "D" => 500,
+            "M" => 1000,
+        ];
+
+        $total = 0;
+        foreach ($values as $word => $value) {
+            if (strpos($s, $word) !== false) {
+                $total += $value;
+                $s = str_replace($word, "", $s);
+            }
+        }
+
+        return $total;
+    }
+}
