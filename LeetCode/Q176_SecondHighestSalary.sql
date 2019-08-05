2817bb000751544b8849a6105b9af4c27c83ed06
Leo.Kuo <et282523@hotmail.com>
2019-08-05 18:32:19 +0800

    Q176 - submit $ - Accepted(看解答)

--- LeetCode/Q176_SecondHighestSalary.sql
+++ LeetCode/Q176_SecondHighestSalary.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-SELECT IFNULL(Salary, NULL) AS SecondHighestSalary FROM (SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1,1) AS sal
+SELECT IFNULL((SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1,1), NULL) AS SecondHighestSalary
+
+
