8067d520272df8652a98b3c52982dfdd92423683
Leo.Kuo <et282523@hotmail.com>
2019-08-05 16:11:03 +0800

    Q13 - submit 3 - Accepted (調效能)

--- LeetCode/Q13_RomanToInteger_Test.php
+++ LeetCode/Q13_RomanToInteger_Test.php
@@ -66,43 +66,61 @@ class Solution
     public function romanToInt($s)
     {
         $values = [
-            "VIII" => 8,
-            "LXXX" => 80,
-            "DCCC" => 800,
-            "III" => 3,
-            "VII" => 7,
-            "XXX" => 30,
-            "LXX" => 70,
-            "CCC" => 300,
-            "DCC" => 700,
-            "MMM" => 3000,
-            "II" => 2,
-            "IV" => 4,
-            "VI" => 6,
-            "IX" => 9,
-            "XX" => 20,
-            "XL" => 40,
-            "LX" => 60,
-            "XC" => 90,
-            "CC" => 200,
-            "CD" => 400,
-            "DC" => 600,
-            "CM" => 900,
-            "MM" => 2000,
-            "I" => 1,
-            "V" => 5,
-            "X" => 10,
-            "L" => 50,
-            "C" => 100,
-            "D" => 500,
-            "M" => 1000,
+            // 個
+            [
+                "VIII" => 8,
+                "III" => 3,
+                "VII" => 7,
+                "II" => 2,
+                "IV" => 4,
+                "VI" => 6,
+                "IX" => 9,
+                "I" => 1,
+                "V" => 5,
+            ],
+            // 十
+            [
+                "LXXX" => 80,
+                "XXX" => 30,
+                "LXX" => 70,
+                "XX" => 20,
+                "XL" => 40,
+                "LX" => 60,
+                "XC" => 90,
+                "X" => 10,
+                "L" => 50,
+            ],
+            // 百
+            [
+                "DCCC" => 800,
+                "CCC" => 300,
+                "DCC" => 700,
+                "CC" => 200,
+                "CD" => 400,
+                "DC" => 600,
+                "CM" => 900,
+                "C" => 100,
+                "D" => 500,
+            ],
+            // 千
+            [
+                "MMM" => 3000,
+                "MM" => 2000,
+                "M" => 1000,
+            ],
         ];
 
         $total = 0;
-        foreach ($values as $word => $value) {
-            if (strpos($s, $word) !== false) {
-                $total += $value;
-                $s = str_replace($word, "", $s);
+        foreach ($values as $groups => $group) {
+            foreach ($group as $word => $value) {
+                if (strpos($s, $word) !== false) {
+                    $total += $value;
+                    $s     = str_replace($word, "", $s);
+                    if ($s == "") {
+                        break 2;
+                    }
+                    break;
+                }
             }
         }
 
