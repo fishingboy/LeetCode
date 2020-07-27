bb4d35d8f96e923f3bae3b1643f39b9928429004
Leo.Kuo <et282523@hotmail.com>
2020-07-27 18:32:53 +0800

    Q14 - Submit1 - WA -- Input    ["a"] Output   "" Expected "a"

---
+++ LeetCode/Q14_LongestCommonPrefix_Test.php
@@ -0,0 +1,57 @@
+<?php
+namespace LeetCode\Q14;
+use PHPUnit\Framework\TestCase;
+
+class Q14_LongestCommonPrefix_Test extends TestCase
+{
+    /**
+     * @var Solution
+     */
+    private $solution;
+
+    public function setUp()
+    {
+        $this->solution = new Solution();
+    }
+
+    public function test_sample1()
+    {
+        $strs = ["flower","flow","flight"];
+        $response = $this->solution->longestCommonPrefix($strs);
+        $this->assertEquals("fl", $response);
+    }
+
+    public function test_sample2()
+    {
+        $strs = ["dog","racecar","car"];
+        $response = $this->solution->longestCommonPrefix($strs);
+        $this->assertEquals("", $response);
+    }
+}
+
+class Solution
+{
+    /**
+     * @param String[] $strs
+     * @return String
+     */
+    function longestCommonPrefix($strs)
+    {
+        $answer = "";
+        $first_len = strlen($strs[0]);
+        $count = count($strs);
+        for ($i=0; $i<$first_len; $i++) {
+            for ($j = 2; $j < $count; $j++) {
+                if ($strs[$j][$i] != $strs[0][$i]) {
+                    break;
+                }
+            }
+
+            if ($j == $count) {
+                $answer .= $strs[0][$i];
+            }
+        }
+
+        return $answer;
+    }
+}
