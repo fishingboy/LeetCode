cd31f6625b3ec9d68baa2e18c5291bdcfc231d30
Leo.Kuo <et282523@hotmail.com>
2020-07-31 00:41:49 +0800

    Q1441 - Submit 1 - Accepted (faster then 100%)

---
+++ LeetCode/Q1441_Build_an_ArrayWithStackOperations_Test.php
@@ -0,0 +1,68 @@
+<?php
+namespace LeetCode\Q1441;
+use PHPUnit\Framework\TestCase;
+
+class Q1441_Build_an_ArrayWithStackOperations_Test extends TestCase
+{
+    /**
+     * @var Solution
+     */
+    private $solution;
+
+    public function setUp()
+    {
+        $this->solution = new Solution();
+    }
+
+    public function testSample1()
+    {
+        $target = [1,3];
+        $n = 3;
+        $response = $this->solution->buildArray($target, $n);
+        $this->assertArraySubset(["Push","Push","Pop","Push"], $response);
+    }
+
+    public function testSample2()
+    {
+        $target = [1,2,3];
+        $n = 3;
+        $response = $this->solution->buildArray($target, $n);
+        $this->assertArraySubset(["Push","Push","Push"], $response);
+    }
+
+    public function testSample3()
+    {
+        $target = [1,2];
+        $n = 4;
+        $response = $this->solution->buildArray($target, $n);
+        $this->assertArraySubset(["Push","Push"], $response);
+    }
+}
+
+class Solution
+{
+    /**
+     * @param Integer[] $target
+     * @param Integer $n
+     * @return String[]
+     */
+    function buildArray($target, $n)
+    {
+        $answer = [];
+
+        $i = 0;
+        foreach ($target as $item) {
+            while ($i < $item) {
+                $i++;
+                if ($item == $i) {
+                    $answer[] = "Push";
+                } else {
+                    $answer[] = "Push";
+                    $answer[] = "Pop";
+                }
+            }
+        }
+
+        return $answer;
+    }
+}
