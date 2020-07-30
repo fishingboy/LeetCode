e5744be87c6371e2d5d6732f6521b8bdb180f7ec
Leo.Kuo <et282523@hotmail.com>
2020-07-30 23:32:46 +0800

    Q137 - Submit 1 - Accepted (faster then 27.27%%)

---
+++ LeetCode/Q137_SingleNumberII_Test.php
@@ -0,0 +1,56 @@
+<?php
+namespace LeetCode\Q137;
+use PHPUnit\Framework\TestCase;
+
+class Q137_SingleNumberII_Test extends TestCase
+{
+    /**
+     * @var Solution
+     */
+    private $solution;
+
+    public function setUp()
+    {
+        $this->solution = new Solution();
+    }
+
+    public function testSample1()
+    {
+        $nums = [2,2,3,2];
+        $response = $this->solution->singleNumber($nums);
+        $this->assertEquals(3, $response);
+    }
+
+    public function testSample2()
+    {
+        $nums = [0,1,0,1,0,1,99];
+        $response = $this->solution->singleNumber($nums);
+        $this->assertEquals(99, $response);
+    }
+}
+
+class Solution
+{
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function singleNumber($nums)
+    {
+        $map = [];
+        foreach ($nums as $item) {
+            if (isset($map[$item])) {
+                $map[$item]["lack"]--;
+                if ($map[$item]["lack"] == 0) {
+                    unset($map[$item]);
+                }
+            } else {
+                $map[$item] = [
+                    "value" => $item,
+                    "lack" => 2,
+                ];
+            }
+        }
+        return array_pop($map)["value"];
+    }
+}
