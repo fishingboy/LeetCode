5f812636efde410c52bec5a76fc3bd3b798e0028
Leo.Kuo <et282523@hotmail.com>
2020-07-30 23:21:30 +0800

    Q136 - Submit 1 - Accepted (faster then 21.60%)

---
+++ LeetCode/Q136_SingleNumber_Test.php
@@ -0,0 +1,50 @@
+<?php
+namespace LeetCode\Q136;
+use PHPUnit\Framework\TestCase;
+
+class Q136_SingleNumber_Test extends TestCase
+{
+    /**
+     * @var Solution
+     */
+    private $solution;
+
+    public function setUp()
+    {
+        $this->solution = new Solution();
+    }
+
+    public function testSample1()
+    {
+        $nums = [2,2,1];
+        $response = $this->solution->singleNumber($nums);
+        $this->assertEquals(1, $response);
+    }
+
+    public function testSample2()
+    {
+        $nums = [4,1,2,1,2];
+        $response = $this->solution->singleNumber($nums);
+        $this->assertEquals(4, $response);
+    }
+}
+
+class Solution
+{
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function singleNumber($nums)
+    {
+        $map = [];
+        foreach ($nums as $item) {
+            if (isset($map[$item])) {
+                unset($map[$item]);
+            } else {
+                $map[$item] = $item;
+            }
+        }
+        return array_pop($map);
+    }
+}
