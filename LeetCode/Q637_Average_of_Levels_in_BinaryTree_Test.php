f1e0162abeebd779059f38171bfb1138f4cb501d
Leo.Kuo <et282523@hotmail.com>
2020-09-08 22:11:08 +0800

    Q637 - Submit1 - Accepted(20.00%)

---
+++ LeetCode/Q637_Average_of_Levels_in_BinaryTree_Test.php
@@ -0,0 +1,158 @@
+<?php
+namespace LeetCode\Q637;
+use PHPUnit\Framework\TestCase;
+
+class Q637_Average_of_Levels_in_BinaryTree_Test extends TestCase
+{
+    /**
+     * @var Solution
+     */
+    private $solution;
+
+    public function setUp()
+    {
+        $this->solution = new Solution();
+    }
+
+    public function testSample1()
+    {
+        $tree = [3,9,20,null,null,15,7];
+        $root = $this->buildTree($tree);
+        $response = $this->solution->averageOfLevels($root);
+        echo "<pre>response = " . print_r($response, true) . "</pre>\n";
+        $this->assertArraySubset([3, 14.5, 11], $response);
+    }
+
+    public function test_1()
+    {
+        $tree = [1,2,3];
+        $root = $this->buildTree($tree);
+        $response = $this->solution->averageOfLevels($root);
+        echo "<pre>response = " . print_r($response, true) . "</pre>\n";
+        $this->assertArraySubset([
+            1,
+            2.5,
+        ], $response);
+    }
+
+    public function test_2()
+    {
+        $tree = [1,2];
+        $root = $this->buildTree($tree);
+        $response = $this->solution->averageOfLevels($root);
+        echo "<pre>response = " . print_r($response, true) . "</pre>\n";
+        $this->assertArraySubset([
+            1,
+            2,
+        ], $response);
+    }
+
+    public function test_wa1()
+    {
+        $tree = [];
+        $root = $this->buildTree($tree);
+        $response = $this->solution->averageOfLevels($root);
+        echo "<pre>response = " . print_r($response, true) . "</pre>\n";
+        $this->assertArraySubset([
+        ], $response);
+    }
+
+    public function buildTree($nums)
+    {
+        $num = array_shift($nums);
+        $root = new TreeNode($num);
+        $node_queue[] = $root;
+        while (count($nums)) {
+            $node = array_shift($node_queue);
+            $left_node = new TreeNode(array_shift($nums));
+            $right_node = new TreeNode(array_shift($nums));
+            $node->left = $left_node;
+            $node->right = $right_node;
+            $node_queue[] = $left_node;
+            $node_queue[] = $right_node;
+        }
+        return $root;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution
+{
+    /**
+     * @param TreeNode $root
+     * @return Float[]
+     */
+    function averageOfLevels($root)
+    {
+        if ( ! $root) {
+            return [];
+        }
+
+        $answer = [
+            $root->val
+        ];
+        $node_queue[] = $root;
+        while (count($node_queue)) {
+
+            // 取出同層的值
+            $level_nums = [];
+            $next_queue = [];
+            while ($node = array_shift($node_queue)) {
+                if ($node->left && $node->left->val !== null) {
+                    $level_nums[] = $node->left->val;
+                }
+                if ($node->right && $node->right->val !== null) {
+                    $level_nums[] = $node->right->val;
+                }
+
+                if ($node->left !== null) {
+                    $next_queue[] = $node->left;
+                }
+                if ($node->right !== null) {
+                    $next_queue[] = $node->right;
+                }
+            }
+
+            if (count($level_nums)) {
+                $answer[] = array_sum($level_nums) / count($level_nums);
+
+                // 往下一層走訪
+                $node_queue = $next_queue;
+            }
+        }
+        return $answer;
+    }
+}
+
+class TreeNode
+{
+    public $val = null;
+    public $left = null;
+    public $right = null;
+
+    function __construct($val = 0, $left = null, $right = null)
+    {
+        $this->val   = $val;
+        $this->left  = $left;
+        $this->right = $right;
+    }
+}
+
+/**
+ * Next challenges:
+ * https://leetcode.com/problems/course-schedule-ii/
+ * https://leetcode.com/problems/binary-tree-longest-consecutive-sequence-ii/
+ * https://leetcode.com/problems/escape-a-large-maze/
+ */
