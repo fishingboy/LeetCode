71e061b6d61e32989ad4b17a2bf54e2c6ddc4916
Leo.Kuo <et282523@hotmail.com>
2020-09-08 14:30:41 +0800

    Q101 - Submit2 - Accepted (BFS 練習)

--- LeetCode/Q101_SymmetricTree_Test.php
+++ LeetCode/Q101_SymmetricTree_Test.php
@@ -22,6 +22,22 @@ class Q101_SymmetricTree_Test extends TestCase
         $this->assertTrue($response);
     }
 
+    public function testSample2()
+    {
+        $tree = [1,2,2,null,3,null,3];
+        $root = $this->buildTree($tree);
+        $response = $this->solution->isSymmetric($root);
+        $this->assertFalse($response);
+    }
+
+    public function test_wa1()
+    {
+        $tree = [1,0];
+        $root = $this->buildTree($tree);
+        $response = $this->solution->isSymmetric($root);
+        $this->assertFalse($response);
+    }
+
     public function buildTree($nums)
     {
         $num = array_shift($nums);
@@ -61,7 +77,37 @@ class Solution
      */
     function isSymmetric($root)
     {
+        $node_queue[] = $root;
+        while (count($node_queue)) {
 
+            // 取出同層的值
+            $level_nums = [];
+            $next_queue = [];
+            while ($node = array_shift($node_queue)) {
+                $level_nums[] = $node->left->val ?? null;
+                $level_nums[] = $node->right->val ?? null;
+
+                if ($node->left !== null) {
+                    $next_queue[] = $node->left;
+                }
+                if ($node->right !== null) {
+                    $next_queue[] = $node->right;
+                }
+            }
+
+            // 判斷有沒有對稱
+            while (count($level_nums)) {
+                $head = array_shift($level_nums);
+                $foot = array_pop($level_nums);
+                if ($head !== $foot) {
+                    return false;
+                }
+            }
+
+            // 往下一層走訪
+            $node_queue = $next_queue;
+        }
+        return true;
     }
 }
 
