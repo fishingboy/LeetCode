b2e18f60429c33697a7c397e204425510f0de4cb
Leo.Kuo <et282523@hotmail.com>
2020-10-15 02:29:28 +0800

    Q118 - Submit2 - Accepted(88.24%)

---
+++ LeetCode/Q118_Pascal_Triangle_Test.php
@@ -0,0 +1,58 @@
+<?php
+namespace LeetCode\Q118;
+use PHPUnit\Framework\TestCase;
+
+class Q118_Pascal_Triangle_Test extends TestCase
+{
+    /**
+     * @var Solution
+     */
+    private $solution;
+
+    public function setUp()
+    {
+        $this->solution = new Solution();
+    }
+
+    public function testSample1()
+    {
+        $response = $this->solution->generate(5);
+        echo "<pre>response = " . print_r($response, true) . "</pre>\n";
+        $this->assertArraySubset([
+            [1],
+            [1,1],
+            [1,2,1],
+        ],$response);
+    }
+
+    public function test_WA1()
+    {
+        $response = $this->solution->generate(0);
+        echo "<pre>response = " . print_r($response, true) . "</pre>\n";
+        $this->assertArraySubset([],$response);
+    }
+}
+
+class Solution
+{
+    /**
+     * @param Integer $numRows
+     * @return Integer[][]
+     */
+    function generate($numRows)
+    {
+        if ($numRows == 0) {
+            return [];
+        }
+
+        $answer = [[1]];
+        for ($i=1; $i<$numRows; $i++) {
+            for ($j=0; $j<=$i; $j++) {
+                $left_num = $answer[$i-1][$j-1] ?? 0;
+                $right_num = $answer[$i-1][$j] ?? 0;
+                $answer[$i][$j] = $left_num + $right_num;
+            }
+        }
+        return $answer;
+    }
+}
