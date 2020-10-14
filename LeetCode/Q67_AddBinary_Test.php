50f4569dad8d52c548329de95192ebff420ee4bf
Leo.Kuo <et282523@hotmail.com>
2020-10-14 23:23:13 +0800

    Q67 - Submit3 - Accepted(95.04%)

--- LeetCode/Q67_AddBinary_Test.php
+++ LeetCode/Q67_AddBinary_Test.php
@@ -16,6 +16,8 @@ class Q67_AddBinary_Test extends TestCase
 
     public function test_binaryToInt()
     {
+        $this->solution = new Solution_fail();
+
         $a = "11";
         $response = $this->solution->binaryToInt($a);
         $this->assertEquals(3, $response);
@@ -27,6 +29,8 @@ class Q67_AddBinary_Test extends TestCase
 
     public function test_intToBinary()
     {
+        $this->solution = new Solution_fail();
+
         $a = 5;
         $response = $this->solution->intToBinary($a);
         $this->assertEquals("101", $response);
@@ -48,6 +52,14 @@ class Q67_AddBinary_Test extends TestCase
         $this->assertEquals("100", $response);
     }
 
+    public function testSample2()
+    {
+        $a = "1010";
+        $b = "1011";
+        $response = $this->solution->addBinary($a, $b);
+        $this->assertEquals("10101", $response);
+    }
+
     public function test_wa1()
     {
         $a = "0";
@@ -58,6 +70,49 @@ class Q67_AddBinary_Test extends TestCase
 }
 
 class Solution
+{
+    /**
+     * @param String $a
+     * @param String $b
+     * @return String
+     */
+    function addBinary($a, $b)
+    {
+        $a_len = strlen($a);
+        $b_len = strlen($b);
+
+        $max = max($a_len, $b_len);
+        $remaining = 0;
+
+        $answer = "";
+        for ($i=1; $i<=$max; $i++) {
+            $a_bit = $b_bit = 0;
+            if ($i <= $a_len) {
+                $a_bit = intval($a[$a_len - $i]);
+            }
+            if ($i <= $b_len) {
+                $b_bit = intval($b[$b_len - $i]);
+            }
+
+            $sum = $a_bit + $b_bit + $remaining;
+            $answer =  ($sum % 2) . $answer;
+
+            $remaining = intval($sum / 2);
+        }
+        if ($remaining) {
+            $answer = $remaining . $answer;
+        }
+
+        return $answer;
+    }
+}
+
+/**
+ * 要用大數運算的概念去做，不能轉成整數會爆掉
+ * Class Solution_fail
+ * @package LeetCode\Q67
+ */
+class Solution_fail
 {
     /**
      * @param String $a
