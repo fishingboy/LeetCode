f20a2d34c9f47af5585d9140cc30603dd9f89adc
Leo.Kuo <et282523@hotmail.com>
2020-10-15 22:54:07 +0800

    Q171 - Submit2 - Accepted(95.24%)

--- LeetCode/Q171_ExcelSheetColumnNumber_Test.php
+++ LeetCode/Q171_ExcelSheetColumnNumber_Test.php
@@ -45,43 +45,16 @@ class Solution
      */
     function titleToNumber($s)
     {
-        $mapping = [
-            "A" => 1,
-            "B" => 2,
-            "C" => 3,
-            "D" => 4,
-            "E" => 5,
-            "F" => 6,
-            "G" => 7,
-            "H" => 8,
-            "I" => 9,
-            "J" => 10,
-            "K" => 11,
-            "L" => 12,
-            "M" => 13,
-            "N" => 14,
-            "O" => 15,
-            "P" => 16,
-            "Q" => 17,
-            "R" => 18,
-            "S" => 19,
-            "T" => 20,
-            "U" => 21,
-            "V" => 22,
-            "W" => 23,
-            "X" => 24,
-            "Y" => 25,
-            "Z" => 26,
-        ];
-
+        $zero = ord("A") - 1;
         $len = strlen($s);
         $total = 0;
         $base = 1;
         for ($i=$len-1; $i>=0; $i--) {
             $word = $s[$i];
-            $total += $base * $mapping[$word];
+            $total += $base * (ord($word) - $zero);
             $base *= 26;
         }
+
         return $total;
     }
 }
