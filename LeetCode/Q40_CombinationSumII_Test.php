338c99abcd6040b212271e5f3e1dd57342aa479b
Leo.Kuo <et282523@hotmail.com>
2020-11-13 14:08:42 +0800

    Q40 - Submit2 - WA

--- LeetCode/Q40_CombinationSumII_Test.php
+++ LeetCode/Q40_CombinationSumII_Test.php
@@ -14,6 +14,22 @@ class Q40_CombinationSumII_Test extends TestCase
         $this->solution = new Solution();
     }
 
+    public function test_count()
+    {
+        $candidates = [1,2,1,2,2];;
+        $response = $this->solution->countCandidates($candidates);
+        $this->assertArraySubset([
+            [
+                "number" => 1,
+                "quota" => 2,
+            ],
+            [
+                "number" => 2,
+                "quota" => 3,
+            ],
+        ], $response);
+    }
+    
     public function testSample1()
     {
         $candidates = [10,1,2,7,6,1,5]; $target = 8;
@@ -24,6 +40,14 @@ class Q40_CombinationSumII_Test extends TestCase
             [1,7],
             [2,6]
         ], $response);
+    }
+
+    public function testWa1()
+    {
+        $candidates = [1,2]; $target = 4;
+        $response = $this->solution->combinationSum2($candidates, $target);
+        $this->assertArraySubset([], $response);
+        echo "<pre>response = " . print_r($response, true) . "</pre>\n";
     }
 }
 
@@ -41,33 +65,53 @@ class Solution
     public function combinationSum2($candidates, $target)
     {
         sort($candidates);
-        $this->candidates = $candidates;
-        $this->count = count($candidates);
+        $this->candidates = $this->countCandidates($candidates);
+        $this->count = count($this->candidates);
         $this->solutions = [];
         $this->find($target, [], 0);
-
-        // 解開答案
-        $solutions = [];
-        foreach ($this->solutions as $solution) {
-            $solutions[] = explode("#", $solution);
-        }
-        return $solutions;
+        return $this->solutions;
     }
 
     public function find($remaining, $output, $index)
     {
         for ($i=$index; $i < $this->count ;$i++) {
-            $item = $this->candidates[$i];
+            if  ($this->candidates[$i]['quota'] == 0) {
+                continue;
+            }
+
+            $item = $this->candidates[$i]['number'];
             if ($item == $remaining) {
                 // find
                 $output[] = $item;
-                $answer = implode("#",$output);
-                $this->solutions[$answer] = $answer;
+                $this->solutions[] = $output;
                 break;
             } else if ($item < $remaining) {
                 $new_output = array_merge($output, [$item]);
-                $this->find($remaining - $item, $new_output, $i+1);
+                $this->candidates[$index]['quota']--;
+                $this->find($remaining - $item, $new_output, $i);
+                $this->candidates[$index]['quota']++;
             }
         }
+    }
+
+    /**
+     * @param array $candidates
+     * @return array
+     */
+    public function countCandidates(array $candidates)
+    {
+        $count = [];
+        foreach ($candidates as $candidate) {
+            if (!isset($count[$candidate])) {
+                $count[$candidate] = [
+                    "number" => $candidate,
+                    "quota" => 1,
+
+                ];
+            } else {
+                $count[$candidate]["quota"]++;
+            }
+        }
+        return array_values($count);
     }
 }
