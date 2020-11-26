0767be4bb41d096384310d41e54dc5c4cd166915
Leo.Kuo <et282523@hotmail.com>
2020-11-26 17:29:54 +0800

    Q155 - Submit3 - WA

--- LeetCode/Q155_MinStack_Test.php
+++ LeetCode/Q155_MinStack_Test.php
@@ -14,14 +14,63 @@ class Q155_MinStack_Test extends TestCase
         $response = $minStack->getMin(); // return -3
         $this->assertEquals(-3, $response);
 
-        $response = $minStack->pop();
-        $this->assertEquals(-3, $response);
+        $minStack->pop();
 
         $response = $minStack->top();    // return 0
         $this->assertEquals(0, $response);
 
         $response = $minStack->getMin(); // return -2
         $this->assertEquals(-2, $response);
+    }
+
+    public function testSample1_1()
+    {
+        $actions = ["MinStack","push","push","push","getMin","pop","top","getMin"];
+        $params = [[],[-2],[0],[-3],[],[],[],[]];
+        $response = $this->operation($actions, $params);
+        $this->assertArraySubset([null,null,null,null,-3,null,0,-2], $response);
+    }
+
+    public function test_WA1()
+    {
+        $actions = ["MinStack","push","push","push","top","pop","getMin","pop","getMin","pop","push","top","getMin","push","top","getMin","pop","getMin"];
+        $params = [[],[2147483646],[2147483646],[2147483647],[],[],[],[],[],[],[2147483647],[],[],[-2147483648],[],[],[],[]];
+        $response = $this->operation($actions, $params);
+
+        echo "<pre>response = " . print_r($response, true) . "</pre>\n";
+
+        $this->assertArraySubset([null,null,null,null,2147483647,null,2147483646,null,2147483646,null,null,2147483647,2147483647,null,-2147483648,-2147483648,null,2147483647], $response);
+    }
+
+    public function operation($actions, $params)
+    {
+        $minStack = new MinStack();
+
+        $output = [];
+        while (count($actions)) {
+            $action = array_shift($actions);
+            $param = array_shift($params);
+
+            $response = null;
+            switch ($action) {
+                case "MinStack":
+                    break;
+                case "push":
+                    $minStack->push($param[0]);
+                    break;
+                case "pop":
+                    $minStack->pop();
+                    break;
+                case "top":
+                    $response = $minStack->top();
+                    break;
+                case "getMin":
+                    $response = $minStack->getMin();
+                    break;
+            }
+            $output[] = $response;
+        }
+        return $output;
     }
 }
 
@@ -61,7 +110,13 @@ class MinStack
     function pop()
     {
         array_pop($this->min_stack);
-        return array_pop($this->stack);
+        array_pop($this->stack);
+
+        if (count($this->stack) == 0) {
+            $this->curr_min = null;
+        }
+
+        return NULL;
     }
 
     /**
