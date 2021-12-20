7bafede00cbcffa548c21e7429bb074eaebd9933
Leo.Kuo <et282523@hotmail.com>
2021-12-21 00:56:43 +0800

    Q8 - Submit 2 - WA

--- LeetCode/Q8_StringToInteger_Test.php
+++ LeetCode/Q8_StringToInteger_Test.php
@@ -20,6 +20,34 @@ class Q8_StringToInteger_Test extends TestCase
         $response = $this->solution->myAtoi($s);
         $this->assertEquals(42, $response);
     }
+
+    public function testSample2()
+    {
+        $s = "   -42";
+        $response = $this->solution->myAtoi($s);
+        $this->assertEquals(-42, $response);
+    }
+
+    public function testSample3()
+    {
+        $s = "4193 with words";
+        $response = $this->solution->myAtoi($s);
+        $this->assertEquals(4193, $response);
+    }
+
+    public function test_WA1()
+    {
+        $s = "-91283472332";
+        $response = $this->solution->myAtoi($s);
+        $this->assertEquals(-2147483648, $response);
+    }
+
+    public function test_WA2()
+    {
+        $s = "   -115579378e25";
+        $response = $this->solution->myAtoi($s);
+        $this->assertEquals(-115579378, $response);
+    }
 }
 
 class Solution
@@ -28,8 +56,15 @@ class Solution
      * @param String $str
      * @return Integer
      */
-    function myAtoi($str)
+    function myAtoi(string $str): int
     {
-        return intval($str);
+        $answer = intval($str);
+        if ($answer > pow(2, 31) -1) {
+            return pow(2, 31) -1;
+        } else if ($answer <  - 1 * pow(2, 31)) {
+            return - 1 * pow(2, 31);
+        } else {
+            return $answer;
+        }
     }
 }
