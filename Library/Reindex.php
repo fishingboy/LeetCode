18fc61c4c76c4ca35248b42c0292e09b910e775e
Leo Kuo <leokuo@qnap.com>
2022-01-18 17:52:10 +0800

    Reindex 第一版

--- Library/Reindex.php
+++ Library/Reindex.php
@@ -9,6 +9,7 @@ class Reindex
         $files = $this->getFiles();
         $files = $this->getFilesInfo($files);
         echo "<pre>files = " . print_r($files, true) . "</pre>\n";
+        $this->rebuildReadMe($files);
     }
 
     /**
@@ -28,28 +29,59 @@ class Reindex
         return $files;
     }
 
+    /**
+     * 取得檔案資訊
+     * @param $file
+     * @return array
+     */
     public function getFileInfo($file): array
     {
-        $info = [];
+        $info = [
+            "no" => 0,
+            "title" => "",
+            "tag" => "",
+        ];
         $content = file_get_contents($file);
 
         // 題號
-        preg_match("/Q([0-9]+).*\.php/" , $file, $matches);
+        preg_match("/Q([0-9]+)_(.*)_Test.*\.php/" , $file, $matches);
         $info['no'] = intval($matches[1]);
+
+        // 題目
+        $info['title'] = $matches[2];
 
         // TAG
         if (preg_match("/\* @tag (.*)/", $content, $matches)) {
-            $info['tag'] = $matches[1];
+            $info['tag'] = trim($matches[1]);
         }
         return $info;
     }
 
+    /**
+     * 取得所有檔案資訊
+     * @param array $files
+     * @return array
+     */
     public function getFilesInfo(array $files)
     {
         foreach ($files as $key => $file) {
             $files[$key] = $this->getFileInfo($file);
         }
         return $files;
+    }
+
+    public function rebuildReadMe($files)
+    {
+        $content  = "| NO | TITLE | TAG |\n";
+        $content .= "|:----|:----|:----|\n";
+        foreach ($files as $i => $file) {
+            $content .= "| {$file['no']} | {$file['title']} | {$file['tag']} |\n";
+        }
+
+        //
+        file_put_contents("README.md", $content);
+
+        return $content;
     }
 }
 
--- Other/ReindexTest.php
+++ Other/ReindexTest.php
@@ -32,8 +32,18 @@ class ReindexTest extends TestCase
         $reindex = new Reindex();
         $files = $reindex->getFilesInfo([__DIR__ . "/../LeetCode/Q1_TwoSum_Test.php"]);
 
-        echo "<pre>files = " . print_r($files, true) . "</pre>\n";
+//        echo "<pre>files = " . print_r($files, true) . "</pre>\n";
+//        echo "<pre>files = " . json_encode($files, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT) . "</pre>\n";
+        echo "<pre>files = " . json_encode($files) . "</pre>\n";
 
         $this->assertEquals("array", gettype($files));
+    }
+
+    public function testRebuildReadMe()
+    {
+        $files = json_decode('[{"no":1,"tag":"math"}]', true);
+        echo "<pre>files = " . print_r($files, true) . "</pre>\n";
+        $reindex = new Reindex();
+        $reindex->rebuildReadMe($files);
     }
 }
