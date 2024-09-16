2650adddb0906b82cabfef7a858617f52b41c2ed
Leo Kuo <et282523@hotmail.com>
2024-09-17 01:05:23 +0800

    Golang Q58 Accepted

---
+++ GO/Q58_LengthOfLastWord_test.go
@@ -0,0 +1,38 @@
+package LeetCode
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_SearchInsert2(t *testing.T) {
+	// 定義測試案例的結構體
+	type testCase struct {
+		Name string `json:"name"`
+		Args struct {
+			S string `json:"s"`
+		} `json:"args"`
+		Expected int `json:"expected"`
+	}
+
+	// 讀取 JSON 文件內容
+	jsonString, err := readFromFile("../TestData/Q58.json")
+	if err != nil {
+		t.Fatalf("Failed to read JSON file: %v", err)
+	}
+
+	// 解析 JSON 字符串到測試案例結構體 slice 中
+	var tests []testCase
+	err = json.Unmarshal([]byte(jsonString), &tests)
+	if err != nil {
+		t.Fatalf("Failed to unmarshal JSON: %v", err)
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			got := lengthOfLastWord(tt.Args.S)
+			assert.Equal(t, tt.Expected, got, "Test case %s failed", tt.Name)
+		})
+	}
+}
