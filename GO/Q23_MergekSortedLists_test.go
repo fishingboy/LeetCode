1be9ca5dbb9956b12bc29ad7c53f0fc7b16e9b6d
Leo Kuo <et282523@hotmail.com>
2025-11-05 17:53:26 +0800

    Golang Q23 Accepted(但很慢)

---
+++ GO/Q23_MergekSortedLists_test.go
@@ -0,0 +1,98 @@
+package LeetCode
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeKLists(lists []*ListNode) *ListNode {
+	var firstNode, beforeNode, answer *ListNode
+	length := len(lists)
+
+	for {
+		nodes := make([]*ListNode, length)
+		minN := 99999
+		minI := -1
+
+		//fmt.Println("lists =>", lists[0].Val, lists[1].Val, lists[2].Val)
+
+		for i, node := range lists {
+			if node == nil {
+				continue
+			}
+
+			if nodes[i] == nil {
+				nodes[i] = node
+			}
+
+			if nodes[i].Val < minN {
+				minN = nodes[i].Val
+				minI = i
+			}
+		}
+
+		//fmt.Println("nodes =>", nodes[0].Val, nodes[1].Val, nodes[2].Val)
+
+		if minI == -1 {
+			break
+		}
+
+		answer = &ListNode{Val: minN}
+		lists[minI] = lists[minI].Next
+
+		if firstNode == nil {
+			firstNode = answer
+		}
+
+		if beforeNode != nil {
+			beforeNode.Next = answer
+		}
+
+		beforeNode = answer
+	}
+
+	return firstNode
+}
+
+func Test_mergeKLists(t *testing.T) {
+	// 定義測試案例的結構體
+	type testCase struct {
+		Name string `json:"name"`
+		Args struct {
+			Lists [][]int `json:"lists"`
+		} `json:"args"`
+		Expected []int `json:"expected"`
+	}
+
+	// 讀取 JSON 文件內容
+	jsonString, err := readFromFile("../TestData/Q23.json")
+	if err != nil {
+		t.Fatalf("Failed to read JSON file: %v", err)
+	}
+
+	// 解析 JSON 字符串到測試案例結構體 slice 中
+	var tests []testCase
+	err = json.Unmarshal([]byte(jsonString), &tests)
+	if err != nil {
+		t.Fatalf("Failed to unmarshal JSON: %v", err)
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			lists := []*ListNode{}
+			for _, list := range tt.Args.Lists {
+				lists = append(lists, array2List(list))
+			}
+			got := list2array(mergeKLists(lists))
+			assert.Equal(t, tt.Expected, got, "Test case %s failed", tt.Name)
+		})
+	}
+}
