eab3e9b795375c7e5a5aadbb2c598a46117480aa
Leo Kuo <et282523@hotmail.com>
2025-11-09 11:41:50 +0800

    Q3736 Golang Accepted

---
+++ GO/Q3736_MinimumMovesToEqualArrayElementsIII_test.go
@@ -0,0 +1,50 @@
+package LeetCode
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func minMoves(nums []int) int {
+	Max := 0
+	sum := 0
+	for _, num := range nums {
+		if num > Max {
+			Max = num
+		}
+		sum += num
+	}
+	return Max*len(nums) - sum
+}
+
+func Test_minMoves(t *testing.T) {
+	// 定義測試案例的結構體
+	type testCase struct {
+		Name string `json:"name"`
+		Args struct {
+			Nums []int `json:"nums"`
+		} `json:"args"`
+		Expected int `json:"expected"`
+	}
+
+	// 讀取 JSON 文件內容
+	jsonString, err := readFromFile("../TestData/Q3736.json")
+	if err != nil {
+		t.Fatalf("Failed to read JSON file: %v", err)
+	}
+
+	// 解析 JSON 字符串到測試案例結構體 slice 中
+	var tests []testCase
+	err = json.Unmarshal([]byte(jsonString), &tests)
+	if err != nil {
+		t.Fatalf("Failed to unmarshal JSON: %v", err)
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			got := minMoves(tt.Args.Nums)
+			assert.Equal(t, tt.Expected, got, "Test case %s failed", tt.Name)
+		})
+	}
+}
